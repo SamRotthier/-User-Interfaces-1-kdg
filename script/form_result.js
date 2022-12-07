8d75793b5be3d2792f90eb6646042c11af437e3e
IKSAM <sam.rotthier@student.kdg.be>
2022-12-07 23:11:30 +0100

    Commentaar verbeteren aan de solution (regex en submit nog work in progress)

--- script/form_result.js
+++ script/form_result.js
@@ -2,40 +2,22 @@
 const query = window.location.search;
 //splits it into parameters
 const searchParams = new URLSearchParams(query);
+
+function ConfirmationInfo(label){
+    const param = searchParams.get(label);
+    const element = document.getElementById(label);
+    if (element !== null) {
+        element.innerHTML = param;
+    }
+}
+
 //fills in the name
-const paramValueName = searchParams.get("name");
-const element = document.getElementById('name')
-console.log(paramValueName)
-if (paramValueName !== null){
-    element.innerHTML = paramValueName;
-}
-//fills in the number
-const paramValueNumber = searchParams.get("number");
-const element2 = document.getElementById('number')
-if (element2 !== null) {
-    element2.innerHTML = paramValueNumber;
-}
-//fills in the email
-const paramValueMail = searchParams.get("mail");
-const element3 = document.getElementById('mail')
-if (element3 !== null) {
-    element3.innerHTML = paramValueMail;
-}
-//fills in the adress
-const paramValueAddress = searchParams.get("address");
-const element4 = document.getElementById('address')
-if (element4 !== null) {
-    element4.innerHTML = paramValueAddress;
-}
-//fills in the product
-const paramValueProduct = searchParams.get("product");
-const element5 = document.getElementById('product')
-if (element5 !== null) {
-    element5.innerHTML = paramValueProduct;
-}
-//fills in the extra requests
-const paramValueExtra = searchParams.get("request");
-const element6 = document.getElementById('extra')
-if (element6 !== null) {
-    element6.innerHTML = paramValueExtra;
-}
+
+ConfirmationInfo("name");
+ConfirmationInfo("phonenumber");
+ConfirmationInfo("mail");
+ConfirmationInfo("address");
+ConfirmationInfo("product");
+ConfirmationInfo("request");
+
+
--- script/form_validation.js
+++ script/form_validation.js
@@ -1,13 +1,13 @@
 //Starts the scripts on load
-window.addEventListener('load', () => validations())
+window.addEventListener('load', () => validations());
 
 //validation scripts get called here and given the right elements
 function validations(){
     const name = document.getElementById('name');
-    name.addEventListener('blur',() => ValidateName(name))
+    name.addEventListener('blur',() => ValidateName(name));
 
     const email = document.getElementById('mail');
-    email.addEventListener('change',() => ValidateEmail(email))
+    email.addEventListener('input',() => ValidateEmail(email));
 }
 
 //validation of the name
@@ -16,12 +16,14 @@ function ValidateName(name)
     const validationName= /[ ]+[A-z]+|[A-z]+[ ]+/;
     if (validationName.test(name.value))
     {
-        name.classList.toggle('validinputname')
-        return true
+        name.classList.add('validinputname');
+        return true;
     }
-    name.classList.toggle('nonvalidinputname')
-    const element = document.getElementById('errorname')
+    name.classList.add('nonvalidinputname');
+    const element = document.getElementById('errorname');
     element.innerHTML = "you have trailins spaces or an incomplete name";
+    const submit = document.getElementById('submit');
+    submit.preventDefault();
     return false;
 }
 
@@ -31,15 +33,26 @@ function ValidateEmail(mail)
     const validationMailAdress= /^[A-z\-]+\.{1}[A-z\-]+[.{1}[A-z\-]*@(student.)*kdg.be/;
     if (validationMailAdress.test(mail.value))
     {
-        mail.classList.toggle('validinputmail')
+        mail.classList.add('validinputmail');
         return true
     }
-    mail.classList.toggle('nonvalidinputmail')
-    const element = document.getElementById('errormail')
+    mail.classList.add('nonvalidinputmail');
+    const element = document.getElementById('errormail');
     element.innerHTML = "the given mail is not correct";
+    const submit = document.getElementById('submit');
+    submit.preventDefault();
     return false;
 }
 
-
-
-
+function validation (regex,element,errorText,elementById,cssTag ){
+    if (regex.test(element.value))
+    {
+        element.classList.add(cssTag);
+        return true
+    }
+    element.classList.add('nonvalidinputmail');
+    const receivedElement = document.getElementById(elementById);
+    receivedElement.innerHTML = errorText;
+    formSubmit();
+    return false;
+}
