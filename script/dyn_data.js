b1fc96eaa84b06869369dc2aaa8c3b2072b2fa6b
IKSAM <sam.rotthier@student.kdg.be>
2022-12-11 22:22:17 +0100

    Starting the data list

---
+++ script/dyn_data.js
@@ -0,0 +1,173 @@
+const Producten =[
+    Dinos = [
+        {Name: 'Trex',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Trex Skeleton',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'10inch'
+        },
+        {Name: 'Raptor',
+            Description: 'This is a hand drawn illustration of a Raptor.\n' +
+                'It is from the poker card set jurassic Downfall.',
+            Image: '../media/Product_Pictures/600.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'30$',
+            Link:'ProductDetailsPageRaptor.html',
+            Size:'15inch'
+        },
+        {Name: 'Raptor Skeleton',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Therizinosaurus',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'20inch'
+        },
+        {Name: 'Therizonosaurus 2',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'20inch'
+        },
+        {Name: 'Therizinosaurus Skeleton',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Mosasaurus',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Mosasaurus_Skeleton',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Carnotaurus',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+    ],
+    Animals = [
+        {Name: 'Elephant',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'15inch'
+        },
+        {Name: 'Turtle',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Axolotl',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'20inch'
+        },
+        {Name: 'Elephant and Mother',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'20inch'
+        },
+        {Name: 'Red Panda',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        }
+    ],
+    Fantasy = [
+        {Name: 'Knight1',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'15inch'
+        },
+        {Name: 'Knight2',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'20inch'
+        },
+        {Name: 'Knight3',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Knight4',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        },
+        {Name: 'Knight5',
+            Description: '',
+            Image: '',
+            Instructions: '',
+            Price:'',
+            Link:'',
+            Size:'5inch'
+        }
+    ]
+
+]
