91ae3dfd70dde61bb5715d4f6c45c020011f730f
IKSAM <sam.rotthier@student.kdg.be>
2022-12-12 20:04:46 +0100

    data list filling

--- script/dyn_data.js
+++ script/dyn_data.js
@@ -3,25 +3,33 @@ const Categories = ["Dinos", "Animals", "Fantasy"]
 const Products =[
     Dinos = [
         {Name: 'Trex',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
-            Link:'',
+            Description: '\'This is a hand drawn illustration of a Trex.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/trex.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
+            Link:'ProductDetailsPageTrex.html',
             Size:'5inch'
         },
         {Name: 'Trex Skeleton',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
-            Link:'',
+            Description: '\'This is a hand drawn illustration of a Trex Skeleton.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/trex_skeleton.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
+            Link:'ProductDetailsPageTrexSkeleton.html',
             Size:'10inch'
         },
         {Name: 'Raptor',
             Description: 'This is a hand drawn illustration of a Raptor.\n' +
                 'It is from the poker card set jurassic Downfall.',
-            Image: '../media/Product_Pictures/600.png',
+            Image: '../media/Product_Listing_Pictures/raptor.png',
             Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
                 'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
                 'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
@@ -31,142 +39,200 @@ const Products =[
             Size:'15inch'
         },
         {Name: 'Raptor Skeleton',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Raptor Skeleton.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/raptor_skeleton.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Therizinosaurus',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Therizinosaurus.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/therizinosaurus.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'20inch'
         },
         {Name: 'Therizonosaurus 2',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Therizinosaurus.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/Dino_Design_PSD.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'20inch'
         },
         {Name: 'Therizinosaurus Skeleton',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Therizinosaurus Skeleton.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/therizinosaurus_skeleton.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Mosasaurus',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Mosasaurus.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/mosasaurus.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Mosasaurus_Skeleton',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Mosasaurus Skeleton.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/mosasaurus_skeleton.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Carnotaurus',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is a hand drawn illustration of a Carnotaurus.\\n\' +\n' +
+                '                \'It is from the poker card set jurassic Downfall.\'',
+            Image: '../media/Product_Listing_Pictures/king_of_hearts_carnotaurus.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
     ],
     Animals = [
         {Name: 'Elephant',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of an Elephant.\'',
+            Image: '../media/Product_Listing_Pictures/Elephant_Realistic.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'15inch'
         },
         {Name: 'Turtle',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Turtle.\'',
+            Image: '../media/Product_Listing_Pictures/Turtle_PNG.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Axolotl',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of an Axolotl.\'',
+            Image: '../media/Product_Listing_Pictures/Axolotl_PNG.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'20inch'
         },
         {Name: 'Elephant and Mother',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of an Elephant and its mother.\'',
+            Image: '../media/Product_Listing_Pictures/Mother_Child.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'20inch'
         },
         {Name: 'Red Panda',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Red Panda.\'',
+            Image: '../media/Product_Listing_Pictures/RedPanda_PNG.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         }
     ],
     Fantasy = [
         {Name: 'Knight1',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Knight.\'',
+            Image: '../media/Product_Listing_Pictures/king_of_diamonds.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'15inch'
         },
         {Name: 'Knight2',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Knight.\'',
+            Image: '../media/Product_Listing_Pictures/jack_of_clubs.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'20inch'
         },
         {Name: 'Knight3',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Knight.\'',
+            Image: '../media/Product_Listing_Pictures/jack_diamonds.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Knight4',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Knight.\'',
+            Image: '../media/Product_Listing_Pictures/queen_diamonds.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         },
         {Name: 'Knight5',
-            Description: '',
-            Image: '',
-            Instructions: '',
-            Price:'',
+            Description: '\'This is an illustration of a Knight.\'',
+            Image: '../media/Product_Listing_Pictures/queen_hearts.png',
+            Instructions: 'To apply the sticker in a good manner, you first have to find the place where you want to put it.\n' +
+                'Then remove the backing of the sticker and place it carefully on the selected place.\n' +
+                'After that you should press well on it for a few seconds and squeeze the remaining air out of it.\n' +
+                'Now you are all set!',
+            Price:'10$',
             Link:'',
             Size:'5inch'
         }
